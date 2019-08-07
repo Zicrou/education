efb63460d06a628f3b817322386ed6f5df4ab287
Zicrou <aazseck@gmail.com>
2019-08-07 07:26:27 +0000

    Master up to date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
 	include SetSource
 	include CurrentUserConcern
 	include DefaultPageContent
-
-	before_action :set_copyright
-
-	def set_copyright
-		@copyright = ZicrouViewTool::Renderer.copyright 'Zicrou Seck', 'All rights reserved'
-	end
 end
