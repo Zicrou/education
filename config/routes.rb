77d15ef376470588a102eef1ed46da19d5a752c7
Zicrou <aazseck@gmail.com>
2019-08-14 16:37:00 +0000

    Implemented Partials for blog, font awesome and pagination

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+  resources :topics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home'
