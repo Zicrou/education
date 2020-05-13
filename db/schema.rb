1eacaa1713493508461dbad91ab8c42c5426e95b
Zicrou <aazseck@gmail.com>
2020-05-13 04:14:46 +0000

    Removed privileg key in User scheema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_130447) do
+ActiveRecord::Schema.define(version: 2020_05_13_040036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -199,5 +199,4 @@ ActiveRecord::Schema.define(version: 2020_05_06_130447) do
   add_foreign_key "regions", "countries"
   add_foreign_key "seris", "niveaus"
   add_foreign_key "technologies", "portfolios"
-  add_foreign_key "users", "privileges"
 end
