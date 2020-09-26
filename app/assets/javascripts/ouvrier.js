289803391be5ed2e277b0b93c04c8b1d5976a5cb
Zicrou <aazseck@gmail.com>
2020-09-26 01:55:59 +0000

    Clean Up The Ouvrier Js file

--- app/assets/javascripts/ouvrier.js
+++ app/assets/javascripts/ouvrier.js
@@ -1,20 +1,3 @@
-//var opt = region.options[region.selectedIndex]; // reference to selected option
-//console.log( region.value );
-//$(document).ready(function() {
-//    $("#ouvrier_region_id").on('change', function(){
-//        var region = document.getElementById('ouvrier_region_id'); // Get the element by id
-//
-//        $ajax({
-//        url: "ouvriers/index",
-//        type: "GET",
-//        data: {region: $(this).val()},
-//        // Callbacks that will be explained
-//        //success: function(data) {
-//        //$("#staff_member_id").children().remove();
-//      })
-//    })
-//})
-departement = undefined
 function getIDRegion() {
     var regionid = document.getElementById("ouvrier_region_id").value
     let regionID = regionid
