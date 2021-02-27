7003851c6bb458eb051afbaa935992bb4afa0c72
Zicrou <aazseck@gmail.com>
2021-02-27 07:58:14 +0000

    Added Tagging model and Updatet Removetable by droping migration

--- db/migrate/20210218115427_remove_table_by_droping.rb
+++ db/migrate/20210218115427_remove_table_by_droping.rb
@@ -1,15 +1,15 @@
 class RemoveTableByDroping < ActiveRecord::Migration[5.2]
   def change
     #drop_table :mainsdoeuvres
-    #remove_index :metiers, :user_id
-    #remove_column :metiers, :user_id, :bigint
-#
-    #remove_index :ouvriers, :metier_id
-    #remove_column :ouvriers, :metier_id, :bigint
-#
-    #drop_table :metiers
-    #drop_table :ouvriers
-    #drop_table :privileges
-    #drop_table :responszones
+    remove_index :metiers, :user_id
+    remove_column :metiers, :user_id, :bigint
+
+    remove_index :ouvriers, :metier_id
+    remove_column :ouvriers, :metier_id, :bigint
+
+    drop_table :metiers
+    drop_table :ouvriers
+    drop_table :privileges
+    drop_table :responszones
   end
 end
